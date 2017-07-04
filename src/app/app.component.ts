40d6171ae82be7b1ee4eec6f161cabbedeae5e02
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2017-07-04 15:16:02 +1000

    Adding model task.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Task Manager';
+
+  task: Task = new Task(1, 'Send budget to client x');
+}
+
+export class Task {
+  public id: number;
+  public title: string;
+
+  constructor(id: number, title: string) {
+    this.id = id;
+    this.title = title;
+  }
 }
