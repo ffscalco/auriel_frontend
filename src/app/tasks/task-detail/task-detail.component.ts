9f0e6991193a68222ec94c68143b280b417b80de
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2017-07-18 12:23:41 +1000

    Adding switchMap to taskDetail.

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import "rxjs/add/operator/switchMap";
 
 import { Task } from '../shared/task.model';
+import { TaskService } from '../shared/task.service';
 
 @Component({
   selector: 'task-detail',
   templateUrl: './task-detail.component.html'
 })
 
-export class TaskDetailComponent {
-  @Input() public task: Task;
+export class TaskDetailComponent implements OnInit {
+  public task: Task;
+
+  public constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute) { }
+
+  public ngOnInit() {
+    this.route.params
+      .switchMap((params: Params) => this.taskService.getTask(+params['id']))
+      .subscribe(task => this.task = task)
+  }
 }
